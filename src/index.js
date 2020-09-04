03160c23f5b6a096aa9fa720fbf42c843218e54e
brianwlego <legobw@gmail.com>
2020-09-04 18:13:01 -0400

    Added beginning of Search Div

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 
-
+const baseURL = 'http://localhost:3000/'
 const ingredients = 'ingredients'
 const cocktails = 'cocktails'
 
-fetch('http://localhost:3000/' + ingredients)
+const ingreInput = document.querySelector('input#ingredient')
+const searchDiv = document.getElementById('main-search')
+
+const ingredientsArray = []
+
+
+fetch(baseURL + ingredients)
   .then(resp => resp.json())
   .then(fillInDropdown)
   
 
-function fillInDropdown(ingredsArray) {
-  const ingredDD = document.querySelector('#ingredients-dropdown')
-  for (const i of ingredsArray) {
-    ingredDD.insertAdjacentHTML('afterbegin', `
+function fillInDropdown(array) {
+  const ingreDD = document.querySelector('#ingredients-dropdown')
+  for (const i of array) {
+    ingredientsArray.push(i)
+    ingreDD.insertAdjacentHTML('afterbegin', `
       <option value='${i.name}'>
     `)
   }
-  }
+}
 
+ingreInput.addEventListener('change', e => {
+  const found = findIngre(e.target.value)
+  renderIngreToList(found)
+  ingreInput.value = ""
+  renderCocktailDiv(found)
+})
+
+
+//FUNCTIONS FOR ADDING/DELETING INGREDIENTS FROM SEARCH//
+function findIngre(ingreName){
+  return ingredientsArray.find(ingre=>{return ingre.name === ingreName})
+}
+function renderIngreToList(ingreObj){
+  const list = document.getElementById('ingredient-list')
+  list.insertAdjacentHTML('afterbegin', `
+  <li data-id="${ingreObj.id}">
+  ${ingreObj.name}
+  <button id="ingredient-remove">x</button>
+  </li>
+  `)
+}
+searchDiv.addEventListener('click', e => {
+  if (e.target.matches('button#ingredient-remove')){
+    e.target.parentElement.remove()
+  }
+})
+//-------------------------------------------------------//
 
 
